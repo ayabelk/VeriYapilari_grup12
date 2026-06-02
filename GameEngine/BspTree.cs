6b95253260a6c6c697bf70a70ba0cc44a5cfbcb4
rojintopuz <146137876+rojintopuz@users.noreply.github.com>
2026-06-03 01:36:13 +0300

    Remove CollectAlongRay method from BspTree
    Removed the CollectAlongRay method that collects wall segments along a ray.

--- GameEngine/BspTree.cs
+++ GameEngine/BspTree.cs
@@ -93,15 +93,7 @@ namespace GameEngine.DataStructures
             return result;
         }
 
-        public List<WallSegment> CollectAlongRay(float startX, float startY, float endX, float endY)
-        {
-            List<WallSegment> result = new List<WallSegment>();
-            // Işının başlangıç noktasına göre ağacı tarayıp duvarları topluyoruz
-            TraverseNode(_root, startX, startY, result);
-            return result;
-        }
-
-        
+     
         private void TraverseNode(BspNode node, float vx, float vy,
                                    List<WallSegment> result)
         {
